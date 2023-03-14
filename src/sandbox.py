480912d627b588c801e40cd2a58f61d5406adbc3
Lewis-McKenzie <53063230+Lewis-McKenzie@users.noreply.github.com>
2023-03-14 11:43:54 +0000

    embed polarity, category, and aspect

--- src/sandbox.py
+++ src/sandbox.py
@@ -1,41 +1,28 @@
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import tensorflow as tf
-from typing import Set, List
 
 from utils import Loader, Preprocessor, DIR
 from models import BasicModel
 
-#Spacy
-import spacy
-nlp = spacy.load('en_core_web_sm')
-
 df = Loader.load(DIR)
-#Preprocessor.process_all(df)
+x, y = Preprocessor.process_all(df)
 
 model = BasicModel()
 
-txt = df["text"][3]
-print(df.shape)
-print(df["text"])
 model.adapt_encoder(df["text"])
 model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
 
-x, y = Preprocessor.process_all(df)
-
-test_aspect_terms = []
-for review in nlp.pipe(df["text"]):
-    chunks = [(chunk.root.text) for chunk in review.noun_chunks if chunk.root.pos_ == 'NOUN']
-    test_aspect_terms.append(chunks)
-
 model.fit(df["text"], y, epochs=10, verbose=1)
 
-print(test_aspect_terms[0])
-print(y[0], len(y))
-print(df.shape)
+INDEX = 0
+
+txt: str = df["text"][INDEX]
+aspects = [opinion["target"] for opinion in df["opinions"][INDEX]]
+categories = [opinion["category"] for opinion in df["opinions"][INDEX]]
 print(txt)
-print(model.predict(np.array([txt])), y[3])
+print(aspects)
+print(categories)
+print(model.predict(np.array([txt.lower()])))
+print(y[INDEX])
 
--- src/utils/preprocessor.py
+++ src/utils/preprocessor.py
@@ -3,6 +3,12 @@ import pandas as pd
 import tensorflow as tf
 from sklearn.preprocessing import LabelEncoder
 
+import nltk
+nltk.download('stopwords', quiet=True)
+nltk.download('punkt', quiet=True)
+
+MAX_LEN = 96
+
 class Preprocessor:
     label_encoder: LabelEncoder
     categories: Set[str]
@@ -20,10 +26,6 @@ class Preprocessor:
         return text, tf.convert_to_tensor(opinion)
 
     @staticmethod
-    def initialize_label_encoder() -> None:
-        Preprocessor.label_encoder = LabelEncoder().fit(list(Preprocessor.categories))
-
-    @staticmethod
     def initialize_categories(opinions: List[List[Dict[str, str]]]) -> None:
         Preprocessor.categories = set()
         for opinion in opinions:
@@ -31,11 +33,47 @@ class Preprocessor:
                 Preprocessor.categories.add(entry["category"])
 
     @staticmethod
-    def process(x: str, y: List[Dict[str, str]]) -> Tuple[str, List[float]]:
-        return x.lower(), Preprocessor.embed_opinions(y)
+    def initialize_label_encoder() -> None:
+        Preprocessor.label_encoder = LabelEncoder().fit(list(Preprocessor.categories))
 
     @staticmethod
-    def embed_opinions(opinions: List[Dict[str, str]]) -> List[float]:
+    def process(x: str, y: List[Dict[str, str]]) -> Tuple[str, List[float]]:
+        embedded_aspect = Preprocessor.embed_aspects(x, y)
+        embedded_category = Preprocessor.embed_categories(y)
+        embedded_opinions = Preprocessor.embed_opinions(x, y)
+        return x.lower(), embedded_category
+
+    @staticmethod
+    def embed_opinions(text: str, opinions: List[Dict[str, str]]) -> List[List[float]]:
+        embedding = [[0.0 for _ in range(MAX_LEN)] for _ in Preprocessor.categories]
+        sentence = nltk.word_tokenize(text.lower())
+        for opinion in opinions:
+            aspect = nltk.word_tokenize(opinion["target"].lower())
+            category_index = Preprocessor.label_encoder.transform([opinion["category"]])[0]
+            print(Preprocessor.label_encoder.transform([opinion["category"]]))
+            polarity = opinion["polarity"]
+            if polarity == "positive":
+                aspect_start_value = 2.5
+                aspect_continued_value = 1.5
+            elif polarity == "negative":
+                aspect_start_value = -2.5
+                aspect_continued_value = -1.5
+            else:
+                aspect_start_value = 0.5
+                aspect_continued_value = -0.5
+            count = 0
+            for i, word in enumerate(sentence):
+                if word == aspect[0] and count == 0:
+                    embedding[category_index][i] = aspect_start_value
+                    count += 1
+                elif count > 0 and count < len(aspect):
+                    embedding[category_index][i] = aspect_continued_value
+                    count += 1
+        return embedding
+
+
+    @staticmethod
+    def embed_categories(opinions: List[Dict[str, str]]) -> List[float]:
         encoded_opinions = []
         for opinion in opinions:
             encoded_label = Preprocessor.label_encoder.transform([opinion["category"]])
@@ -46,8 +84,28 @@ class Preprocessor:
     @staticmethod
     def merge_encodings(encodings: List[List[float]]) -> List[float]:
         if encodings == []:
-            return [0.0 for _, _ in enumerate(Preprocessor.categories)]
+            return [0.0 for _ in Preprocessor.categories]
         return [max([encoding[i] for encoding in encodings]) for i, _ in enumerate(Preprocessor.categories)]
+
+    @staticmethod
+    def embed_aspects(text: str, opinions: List[Dict[str, str]]) -> List[float]:
+        sentence = nltk.word_tokenize(text.lower())
+        embedded_aspect = [0.0 for _ in sentence]
+        for opinion in opinions:
+            aspect = nltk.word_tokenize(opinion["target"].lower())
+            count = 0
+            for i, word in enumerate(sentence):
+                if word == aspect[0]:
+                    embedded_aspect[i] = 2.0
+                    count += 1
+                elif count > 0 and count < len(aspect):
+                    embedded_aspect[i] = 1.0
+                    count += 1
+        return Preprocessor.pad_aspect(embedded_aspect)
+
+    @staticmethod
+    def pad_aspect(embedded_aspect: List[float]) -> List[float]:
+        return [embedded_aspect[i] if i < len(embedded_aspect) else 0.0 for i in range(MAX_LEN)]
 
     @staticmethod
     def tokenize(text: str) -> List[str]:
@@ -55,11 +113,6 @@ class Preprocessor:
 
     @staticmethod
     def embed_words(words: List[str]) -> List[float]:
-        return []
-
-
-    @staticmethod
-    def pad(embedded_words: List[int]) -> List[int]:
         return []
 
     @staticmethod
