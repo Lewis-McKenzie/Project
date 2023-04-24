75447ef7f3c315ea76f215aee2368eaee3b485d0
Lewis-McKenzie <53063230+Lewis-McKenzie@users.noreply.github.com>
2023-04-24 13:04:32 +0100

    dummy model works

--- src/make_dummy_sandbox.py
+++ src/make_dummy_sandbox.py
@@ -11,25 +11,20 @@ from test_sandbox import DIR, MODEL_WEIGHTS, EMB_PATH
 def main() -> None:    
     df = Loader.load(DIR)
     processor = Preprocessor(df)
-    train_dataset, validation_dataset = processor.make_polarity_category_dataset(16)
-
-    embeddings_index = Loader.load_word_embedings(EMB_PATH)
     LR = 0.005
-    EPOCHS = 1
 
-    model = DummyModel(processor.polarity_category_encoder, len(processor.get_vocab()))
-    model.adapt_encoder(df["text"])
-    model.init_embeddings(embeddings_index)
+    model = DummyModel(processor.polarity_category_encoder)
     ALPHA = 0.5
     model.compile(loss='binary_crossentropy',
                 optimizer=tf.keras.optimizers.Adam(learning_rate=LR),
                 metrics=['binary_accuracy', tf.keras.metrics.Precision(thresholds=ALPHA), tf.keras.metrics.Recall(thresholds=ALPHA), tf.keras.metrics.F1Score(threshold=ALPHA)])
-    model.fit(train_dataset, validation_data=validation_dataset, epochs=EPOCHS, verbose=1)
-    model.save_model(MODEL_WEIGHTS)
+    labels = tf.ragged.constant(processor.polarity_category_values(df))
+    encoded_labels = model.encoder(labels).numpy()
+    model.evaluate(df["text"], encoded_labels)
 
     INDEX = 0
 
-    predict = model.predict(tf.convert_to_tensor(df["text"]))
+    predict = model.predict(df["text"])
     print(df["text"][INDEX])
     print(processor.get_encoded_labels()[INDEX])
     print(predict[INDEX])
--- src/models/dummy_model.py
+++ src/models/dummy_model.py
@@ -3,30 +3,15 @@ import tensorflow as tf
 import numpy as np
 from .basic_model import BasicModel
 
-rng = np.random.default_rng(12345)
-
 class DummyModel(BasicModel):
-    def __init__(self, encoder: tf.keras.layers.StringLookup, vocab_size=1024, embedding_size=100):
+    def __init__(self, encoder: tf.keras.layers.StringLookup):
         super(BasicModel, self).__init__()
         self.encoder = encoder
-        self.vocab_size = vocab_size
-        self.embedding_size = embedding_size
         self.pipeline = tf.keras.Sequential([
-            tf.keras.layers.TextVectorization(
-                max_tokens=vocab_size,
-            ),
-            tf.keras.layers.Embedding(
-                input_dim=vocab_size,
-                output_dim=embedding_size,
-                mask_zero=True,
-            ),
-            tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(1)),
-            tf.keras.layers.Dense(self.encoder.vocabulary_size(), activation='sigmoid', input_shape=(6000,)),
+            tf.keras.layers.Lambda(lambda x: tf.where(tf.random.uniform((tf.shape(x)[0], 37)) < 1/37, 1.0, 0.0)),
         ])
 
     def call(self, inputs, training=None, mask=None):
-        #return [1.0 if rng.random() < 1.0/36 else 0.0 for _ in range(self.encoder.vocabulary_size())]
-        print(self.pipeline(inputs))
         return self.pipeline(inputs)
 
     def save_model(self, path: str) -> None:
